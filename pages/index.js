57d558996d9326fe89c27fec535b299099c45a3b
Nguyễn Mạnh Tuấn <azerprojects@gmail.com>
2020-12-24 15:43:07 +0700

    test deploy workflow

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Yay! Github Actions works successfully
         </h1>
 
         <p className={styles.description}>
