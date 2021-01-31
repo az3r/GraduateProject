acd07e43e1b55fd04dfb7a1ee3bd380f9f66886c
Nguyen Thanh Tung <nguyenthanhtung884318@gmail.com>
2021-01-31 11:34:58 +0700

    Fix Home page, Examiner và Làm bài

--- pages/examiner/add.jsx
+++ pages/examiner/add.jsx
@@ -2,22 +2,23 @@ import { Box, Container, Typography } from '@material-ui/core';
 import React,{useState} from 'react';
 import MyAppBar from '../../components/AppBar';
 import dynamic from 'next/dynamic';
+import Examiner from "../../components/Examiner";
+import Layout from "../../components/Layout";
 
 const AddTest = dynamic(
-    () => {
-      return import('../../components/AddTest');
-    },
-    { ssr: false }
-  );
+  () => {
+    return import('../../components/AddTest');
+  },
+  { ssr: false }
+);
 
 
 export default function AddTestPage(props){
-    return(
-        <>
-            <MyAppBar isExaminer = {true}></MyAppBar>
-            <Container>
-                <AddTest></AddTest>
-            </Container>
-        </>
-    );
+  return(
+    <>
+      <Layout>
+        <AddTest></AddTest>
+      </Layout>
+    </>
+  );
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,18 +1,40 @@
-import { Container, Typography } from "@material-ui/core";
 import Head from "next/head";
-import MyAppBar from "../components/AppBar";
-export default function HomePage() {
+import { Grid } from "@material-ui/core";
+
+
+import TopScore from "../components/TopScore";
+import ProblemsTable from "../components/ProblemsTable";
+import Search from "../components/Search";
+import Layout from "../components/Layout";
+
+
+export default function Home() {
+
   return (
     <>
       <Head>
-        <title>HCMUSCoder - Home</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Smart Coder</title>
+        <link rel="icon" href="/favicon.ico"/>
       </Head>
 
-      <MyAppBar isExaminer={true}></MyAppBar>
-      <Container>
-         
-      </Container>
+      <Layout>
+        <Grid container spacing={3}>
+          <Grid item xs={12}
+                container
+                spacing={0}
+                direction="column"
+                alignItems="center"
+                justify="center">
+            <Search />
+          </Grid>
+          <Grid item xs={12} sm={3}>
+            <TopScore />
+          </Grid>
+          <Grid item xs={12} sm={9}>
+            <ProblemsTable />
+          </Grid>
+        </Grid>
+      </Layout>
     </>
   );
 }
