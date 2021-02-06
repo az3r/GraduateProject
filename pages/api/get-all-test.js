2181044ffa3d912fdebfdad05847110fc0a2e5de
chuberacroi412 <37958877+chuberacroi412@users.noreply.github.com>
2021-02-06 20:24:28 +0700

    api get all exam

---
+++ pages/api/get-all-test.js
@@ -0,0 +1,18 @@
+import admin from '../../libs/firebase_server'
+import createTestModel from '../../model/ExamModel'
+
+const database = admin.firestore()
+
+export default async function getAllTest(req, res)
+{
+    let testData = []
+
+    const dataRef = await database.collection('Exam').get()
+    dataRef.docs.forEach(doc => testData.push(doc.data()))
+
+    if(testData.length > 0)
+        res.status(200).json(testData)
+    else
+        res.status(500).json({message: 'can not get data from firestore'})
+    
+}
