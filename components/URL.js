fd129378da05211d454b3538ac3e40eaf22267cb
tuan <adgtuan@gmail.com>
2021-02-07 12:23:25 +0700

    sửa URL.js lần 2

--- components/URL.js
+++ components/URL.js
@@ -8,7 +8,7 @@ class URL{
         {
             return window.location.protocol + "//" + window.location.hostname+":3000/api/";
         }
-        return window.location.protocol + "//" + window.location.hostname+".vercel.app/api/";
+        return window.location.protocol + "//" + window.location.hostname+"/api/";
     }
 }
 
