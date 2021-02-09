e55e71b511c449363515fb4f6018094d0409b6df
azer <azerprojects@gmail.com>
2021-02-09 16:37:30 +0700

    + CodeEditor: language.toLowerCase; remove width, height

--- pages/api/excute-test.js
+++ pages/api/excute-test.js
@@ -1,88 +1,66 @@
-import admin from '../../libs/firebase_server'
-import createHistoryInstance from '../../model/ExamHistoryModel'
-import base64 from 'base-64'
+import admin from '../../libs/firebase_server';
+import createHistoryInstance from '../../model/ExamHistoryModel';
+import base64 from 'base-64';
 
-const database = admin.firestore()
+const database = admin.firestore();
 
-export default async function handler(req, res){
+export default async function handler(req, res) {
+  const testId = req.body.examId;
+  const uuid = req.body.userId;
+  const codeContent = req.body.code;
 
-    const testId = req.body.examId
-    const uuid = req.body.userId
-    const codeContent = req.body.code
+  const baseUrl = 'https://online-code-exercise.herokuapp.com/';
 
-    const baseUrl = 'https://online-code-exercise.herokuapp.com/'
+  const examRef = await database.collection('Exam').doc(testId);
+  const examData = await (await examRef.get()).data();
 
-    const examRef = await database.collection('Exam').doc(testId)
-    const examData = await (await examRef.get()).data()
+  const apiUrl =
+    baseUrl +
+    (examData.language === 'Python' ? 'py3' : examData.language.toLowerCase());
 
-<<<<<<< HEAD
-    const apiUrl = baseUrl + (examData.language == 'python' ? 'py3' : examData.language.toLowerCase())
+  if (!uuid) {
+    res.status(401).json({ error: 'Unauthorized' });
+    return;
+  }
 
-=======
-    const apiUrl = baseUrl + (examData.language === 'Python' ? 'py3' : examData.language.toLowerCase())
-   
->>>>>>> origin/api
+  const respone = await fetch(apiUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: 'Bearer ' + uuid,
+    },
+    body: JSON.stringify({
+      code: codeContent,
+      testcases: examData.cases,
+    }),
+  });
 
-    if(!uuid)
-    {
-        res.status(401).json({'error': 'Unauthorized'})
-        return
+  if (respone.status === 401) res.status(401).json({ error: 'Unauthorized' });
+  else if (respone.status === 400) {
+    const message = await respone.json();
+    res.status(500).json(message);
+  } else if (respone.status !== 200) {
+    const result = await respone.json();
+    res.status(respone.status).json({ message, comeFrom: 'compiler api' });
+  } else if (respone.status === 200) {
+    try {
+      const result = await respone.json();
+      const historyData = createHistoryInstance(
+        uuid,
+        testId,
+        result,
+        codeContent
+      );
+      await database.collection('History').doc().set(historyData);
+
+      res.status(200).json(result);
+    } catch (error) {
+      res
+        .status(200)
+        .json(result, {
+          message: 'failed to log history to database',
+          error: error,
+        });
     }
-
-
-    const respone = await fetch(apiUrl, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization' : 'Bearer ' + uuid
-        },
-        body: JSON.stringify({
-            'code' : codeContent,
-            'testcases' : examData.cases
-        })
-    })
-
-
-    if(respone.status === 401)
-        res.status(401).json({'error': 'Unauthorized'})
-    else if(respone.status === 400)
-    {
-        const message = await respone.json()
-        res.status(500).json(message)
-    }
-    else if(respone.status !== 200)
-    {
-        const result = await respone.json()
-        res.status(respone.status).json({message, comeFrom: 'compiler api'})
-    }
-<<<<<<< HEAD
-    else if(respone.status == 200)
-    {
-        const result = await respone.json()
-        const historyData = createHistoryInstance(uuid, testId, result, codeContent)
-        database.collection('History').doc().set(historyData)
-        .then(() => {
-            res.status(200).json(result)
-        }).then((error) => {
-            res.status(200).json(result, {message: 'failed to log history to database', error: error})
-        })
-    }
-=======
-    else if(respone.status === 200)
-    {     
-        try{
-            const result = await respone.json()
-            const historyData = createHistoryInstance(uuid, testId, result, codeContent)
-            await database.collection('History').doc().set(historyData)
-
-            res.status(200).json(result)         
-        }catch( error){
-            res.status(200).json(result, {message: 'failed to log history to database', error: error})
-            
-        }
-    }       
->>>>>>> origin/api
-    else
-        res.status(500).json({'error': 'server error'})
-
+  } else res.status(500).json({ error: 'server error' });
 }
