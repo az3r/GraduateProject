4baf8fafd16a9534caa8938c14b1fc1bb806ad0e
azer <azerprojects@gmail.com>
2021-02-25 16:36:14 +0700

    update login

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,34 +1,36 @@
+import React from 'react';
 import { Box, Button, makeStyles, Typography } from '@material-ui/core';
-import { FirebaseAuth } from '../libs/firebase_client';
 import Head from 'next/head';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import { FirebaseAuth } from '../libs/firebase_client';
 
-const config = {
-  callbacks: {
-    signInSuccessWithAuthResult: function (authResult, redirectUrl) {
-      // User successfully signed in.
-      // Return type determines whether we continue the redirect automatically
-      // or whether we leave that to developer to handle.
-      return true;
-    },
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignContent: 'center',
+    alignItems: 'center',
+    padding: theme.spacing(2),
   },
-  // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
-  signInFlow: 'popup',
-  signInSuccessUrl: 'https://graduate-project.vercel.app/',
-  signInOptions: [
-    // Leave the lines as is for the providers you want to offer your users.
-    FirebaseAuth.EmailAuthProvider.PROVIDER_ID,
-    FirebaseAuth.GoogleAuthProvider.PROVIDER_ID,
-    FirebaseAuth.FacebookAuthProvider.PROVIDER_ID,
-    FirebaseAuth.GithubAuthProvider.PROVIDER_ID,
-  ],
-  // Terms of service url.
-  tosUrl: 'https://www.example.com',
-  // Privacy policy url.
-  privacyPolicyUrl: 'https://www.example.com',
-};
+}));
 export default function Login() {
+  const router = useRouter();
+  const config = {
+    callbacks: {
+      signInSuccessWithAuthResult() {
+        router.push('/');
+        return false;
+      },
+    },
+    signInFlow: 'popup',
+    signInOptions: [
+      FirebaseAuth.GoogleAuthProvider.PROVIDER_ID,
+      FirebaseAuth.FacebookAuthProvider.PROVIDER_ID,
+      FirebaseAuth.GithubAuthProvider.PROVIDER_ID,
+    ],
+  };
   const styles = useStyles();
   return (
     <Box className={styles.root}>
@@ -49,13 +51,3 @@ export default function Login() {
     </Box>
   );
 }
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignContent: 'center',
-    alignItems: 'center',
-    padding: theme.spacing(2),
-  },
-}));
