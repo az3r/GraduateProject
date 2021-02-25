b591591a68ec40a9c06c9f6ac3b750c838e80692
azer <azerprojects@gmail.com>
2021-02-25 15:27:53 +0700

    testing husky

--- pages/api/[...error].js
+++ pages/api/[...error].js
@@ -1,5 +1,5 @@
 export default function handler(req, res) {
   res.statusCode = 404;
-  res.setHeader("Content-Type", "application/json");
-  res.end(JSON.stringify({ error: "Unknown api endpoint" }));
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify({ error: 'Unknown api endpoint' }));
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,32 +1,33 @@
-import Head from "next/head";
-import { Grid } from "@material-ui/core";
+import React from 'react';
+import Head from 'next/head';
+import { Grid } from '@material-ui/core';
 
-import Problems from "../components/Problems";
-import YourProgress from "../components/YourProgress";
-import Layout from "../components/Layout";
 import Container from '@material-ui/core/Container';
-
+import Problems from '../components/Problems';
+import YourProgress from '../components/YourProgress';
+import Layout from '../components/Layout';
 
 export default function Home() {
-
   return (
     <>
       <Head>
         <title>Smart Coder</title>
-        <link rel="icon" href="/favicon.ico"/>
+        <link rel="icon" href="/favicon.ico" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Layout>
         <Container>
           <Grid container spacing={3}>
-            <Grid item xs={12}
-                  container
-                  spacing={0}
-                  direction="column"
-                  alignItems="center"
-                  justify="center">
-            </Grid>
+            <Grid
+              item
+              xs={12}
+              container
+              spacing={0}
+              direction="column"
+              alignItems="center"
+              justify="center"
+             />
             <Grid item xs={8}>
               <Problems />
             </Grid>
