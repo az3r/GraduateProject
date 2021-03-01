77a40d47a0de8241714faafee6b1e3b62271ffbf
azer <azerprojects@gmail.com>
2021-03-01 14:33:02 +0700

    + add, get problems and test code

---
+++ libs/client/codes.js
@@ -0,0 +1,65 @@
+import { collections, urls } from '@utils/constants';
+import { Firestore, FirebaseAuth } from './firebase_client';
+
+const { users } = collections;
+const { compiler } = urls;
+const langs = {
+  csharp: 'csharp',
+  python: 'py3',
+  java: 'java',
+};
+const statuses = {
+  passed: 'passed',
+  failed: 'failed',
+  error: 'syntax-error',
+};
+
+export default async function test({
+  problemId,
+  problemName,
+  lang,
+  code,
+  testcases,
+  save,
+}) {
+  const { uid } = FirebaseAuth().currentUser;
+  const token = await FirebaseAuth().currentUser.getIdToken(true);
+  const response = await fetch({
+    method: 'POST',
+    url: `${compiler}${langs[lang.toLowerCase()]}`,
+    body: {
+      code,
+      testcases,
+    },
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  const data = response.json();
+  if (response.status === '200') {
+    const { failed } = data;
+    const status = failed > 0 ? statuses.failed : statuses.passed;
+    if (save) {
+      Firestore().collection(users).doc(uid).collection('submissions').set({
+        problemId,
+        problemName,
+        createdOn: Firestore.Timestamp.now(),
+        status,
+        result: data,
+      });
+    }
+    return data;
+  }
+  if (save) {
+    Firestore().collection(users).doc(uid).collection('submissions').set({
+      problemId,
+      problemName,
+      createdOn: Firestore.Timestamp.now(),
+      status: statuses.error,
+      result: data,
+    });
+  }
+  return Promise.reject(data);
+}
---
+++ libs/client/problems.js
@@ -0,0 +1,33 @@
+import { collections } from '@utils/constants';
+import { Firestore } from './firebase_client';
+
+const { problems } = collections;
+
+export default async function create(
+  userId,
+  { title, language, score, content, difficulty, code, cases }
+) {
+  await Firestore().collection(problems).doc().set({
+    title,
+    language,
+    score,
+    content,
+    difficulty,
+    code,
+    cases,
+    owner: userId,
+    createdOn: Firestore.Timestamp.now(),
+  });
+}
+
+export async function get(problemId) {
+  if (problemId) {
+    const snapshot = await Firestore()
+      .collection(problems)
+      .doc(problemId)
+      .get();
+    return snapshot.data();
+  }
+  const snapshot = await Firestore().collection(problems).get();
+  return snapshot.docs.map((item) => item.data());
+}
