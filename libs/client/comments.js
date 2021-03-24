055e9e4b26386147726520d8f7b1b8db6692a727
azer <azerprojects@gmail.com>
2021-03-24 08:56:38 +0700

    feat(api): exam comments

--- libs/client/comments.js
+++ libs/client/comments.js
@@ -2,7 +2,14 @@ import { collections } from '@utils/constants';
 import { transform } from '@utils/firestore';
 import { Firestore } from './firebase';
 
-const { problems, comments, users, problemComments } = collections;
+const {
+  problems,
+  comments,
+  users,
+  problemComments,
+  exams,
+  examComments,
+} = collections;
 export async function createProblemComment(
   problemId,
   { userId, username, avatar, content }
@@ -25,7 +32,33 @@ export async function createProblemComment(
     .doc(userId)
     .collection(problemComments)
     .doc(problemId)
-    .set({ ownerId: userId }, { merge: true });
+    .set({ ownerId: userId });
+  return id;
+}
+
+export async function createExamComment(
+  examId,
+  { userId, username, avatar, content }
+) {
+  const { id } = await Firestore()
+    .collection(exams)
+    .doc(examId)
+    .collection(comments)
+    .add({
+      userId,
+      username,
+      avatar,
+      content,
+      createdOn: Firestore.Timestamp.now(),
+    });
+
+  // add to user's problem comments collection
+  await Firestore()
+    .collection(users)
+    .doc(userId)
+    .collection(examComments)
+    .doc(examId)
+    .update({ ownerId: userId });
   return id;
 }
 
@@ -47,6 +80,24 @@ export async function updateComment(
     });
 }
 
+export async function updateExamComment(
+  examId,
+  commentId,
+  { userId, username, avatar, content }
+) {
+  return Firestore()
+    .collection(exams)
+    .doc(examId)
+    .collection(comments)
+    .doc(commentId)
+    .update({
+      userId,
+      username,
+      avatar,
+      content,
+    });
+}
+
 export async function deleteProblemComment(problemId, commentId) {
   return Firestore()
     .collection(problems)
@@ -56,10 +107,28 @@ export async function deleteProblemComment(problemId, commentId) {
     .delete();
 }
 
+export async function deleteExamComment(examId, commentId) {
+  return Firestore()
+    .collection(exams)
+    .doc(examId)
+    .collection(comments)
+    .doc(commentId)
+    .delete();
+}
 export async function getProblemComments(problemId) {
   const snapshot = await Firestore()
     .collection(problems)
     .doc(problemId)
+    .collection(comments)
+    .get();
+
+  return snapshot.docs.map((doc) => transform({ id: doc.id, ...doc.data() }));
+}
+
+export async function getExamComments(examId) {
+  const snapshot = await Firestore()
+    .collection(exams)
+    .doc(examId)
     .collection(comments)
     .get();
 
@@ -78,8 +147,15 @@ export async function updateUserComments(userId, { username, avatar }) {
     .collection(problemComments)
     .get();
 
+  // get all problems that have user's comments
+  const examIds = await Firestore()
+    .collection(users)
+    .doc(userId)
+    .collection(examComments)
+    .get();
+
   // update all comments
-  const tasks = problemIds.docs.map(async (problem) => {
+  const problemTasks = problemIds.docs.map(async (problem) => {
     const snapshot = await Firestore()
       .collection(problems)
       .doc(problem.id)
@@ -92,5 +168,19 @@ export async function updateUserComments(userId, { username, avatar }) {
       )
     );
   });
-  await Promise.all(tasks);
+
+  const examTasks = examIds.docs.map(async (exam) => {
+    const snapshot = await Firestore()
+      .collection(exams)
+      .doc(exam.id)
+      .collection(comments)
+      .where('userId', '==', userId)
+      .get();
+    await Promise.all(
+      snapshot.docs.map(async (comment) =>
+        comment.ref.update({ username, avatar })
+      )
+    );
+  });
+  await Promise.all(problemTasks, examTasks);
 }
